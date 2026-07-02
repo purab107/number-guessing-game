b526cc8f45a104dc0da697c7b31639b6c7a51c33
Purab Sahare <purabsahare@gmail.com>
2026-07-02 23:51:16 +0530

    guess-list

--- script.js
+++ script.js
@@ -1,25 +1,46 @@
 const form = document.querySelector('#user-input-field')
+const guessListDiv = document.querySelector('#guess-queue')
+const guessQueue = document.createElement('div')
+guessQueue.id = 'list-guess';
 
-
+let randomNumber = Math.floor((Math.random() * 100) + 1)
 
 let guessList = []
 let numOfTrys = 0
 
 
 form.addEventListener("submit", function (event) {
-    event.preventDefault()
-    const taskName = document.getElementById('user-input').value.trim()
-    document.getElementById('user-input').value = '';
+    event.preventDefault();
 
-    if (!taskName) {
-        alert("enter a valid task name")
-        return
+    const input = document.getElementById("user-input").value.trim();
+    const numGuess = Number(input);
+
+    document.getElementById("user-input").value = "";
+
+    if (!Number.isInteger(numGuess) || numGuess < 1 || numGuess > 100) {
+        alert("Please enter a number between 1 and 100");
+        return;
     }
-    console.log(taskName)
-    console.log("Form submitted");
+
+    guessList.push(numGuess);
+    console.log(`Element added: ${numGuess}`);
+    showGuessList();
 });
 
+function showGuessList() {
+    const newGuess = document.createElement('span')
+    newGuess.className = 'guesses'
+    guessList.forEach(num => {
+        newGuess.textContent = num
+        guessQueue.appendChild(newGuess)
+        console.log(`guess queue appened : ${newGuess}`)
+    });
+    guessListDiv.appendChild(guessQueue)
+}
 
-function checkTheGuess() {}
-function validateAnswer() {}
-function init() {}
+
+function checkTheGuess() { }
+function validateAnswer() { }
+function init() { }
+
+showGuessList()
